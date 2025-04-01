253f8311ecc57bbe3addfbf8ef128b452ccd0121
joyewon0705 <77885098+joyewon0705@users.noreply.github.com>
2025-04-01 23:15:10 +0900

    25/04/01

---
+++ programmers/12911.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        int answer = n + 1;
+        int cnt = Integer.bitCount(n);
+        
+        while (true) {
+            if (Integer.bitCount(answer) == cnt) break;
+            answer++;
+        }
+        
+        return answer;
+    }
+}
