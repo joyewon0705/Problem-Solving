da076ed339766b272fbd83bd9bd2db55905e010f
joyewon0705 <77885098+joyewon0705@users.noreply.github.com>
2025-04-15 00:05:46 +0900

    25/04/15

--- BOJ/BOJ_2836.cpp
+++ BOJ/BOJ_2836.cpp
@@ -1,40 +1,38 @@
 #include <iostream>
 #include <algorithm>
-#include <queue>
+#include <vector>
 using namespace std;
 
 typedef pair<int, int> P;
 
 int n, m, a, b;
 long long ans;
-priority_queue<P> pq;
+vector<P> v;
 
 int main(void) {
-	
+
 	scanf("%d %d", &n, &m);
 	for (int i = 0; i < n; i++) {
 		scanf("%d %d", &a, &b);
-		if (a > b) pq.push({ a, b });
+		if (a > b) v.push_back({ b, a });
 	}
 
-	ans = m;
-	a = pq.top().first;
-	b = pq.top().second;
-	while (!pq.empty()) {
-		int x = pq.top().first;
-		int y = pq.top().second;
-		pq.pop();
-
-		if (b > x) {
-			ans += 2 * (a - b);
-			a = x;
-			b = y;
+	sort(v.begin(), v.end());
+	
+	a = b = 0;
+	for (P cur : v) {
+		if (b < cur.first) {
+			ans += b - a;
+			a = cur.first;
+			b = cur.second;
 		}
-		else b = min(b, y);
+		else {
+			if (b < cur.second) b = cur.second;
+		}
 	}
-	ans += 2 * (a - b);
+	ans = m + (ans + b - a) * 2;
 
 	printf("%lld", ans);
-		
+
 	return 0;
 }
